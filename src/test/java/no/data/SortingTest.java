09f5cf1d8e4fbbfa4ff27821bf2864372a73eb3d
victorr <victorr@backbase.com>
2021-09-16 02:59:50 -0500

    :test: define unit tests
    Changes committed:
    	modified:   src/test/java/no/data/SortingTest.java

--- src/test/java/no/data/SortingTest.java
+++ src/test/java/no/data/SortingTest.java
@@ -2,6 +2,7 @@ package no.data;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -9,6 +10,19 @@ import org.junit.Test;
  */
 public class SortingTest
 {
+
+    List<Person> persons;
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    public void whenUsingAgeComparator_thenSortedList(){
+
+    }
+
     /**
      * Rigorous Test :-)
      */
