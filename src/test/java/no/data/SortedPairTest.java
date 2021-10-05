6ce9f7e5602bdaac659f11747b0207a7c2484af6
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-10-05 10:25:36 -0500

    :test: define unit tests

---
+++ src/test/java/no/data/SortedPairTest.java
@@ -0,0 +1,15 @@
+package no.data;
+
+import org.junit.Test;
+
+public class SortedPairTest {
+
+    @Test
+    public void shouldRetainOrderOfOrderedPair(){
+    }
+
+    @Test
+    public void shouldFlipOrderOfUnorderedPair(){
+
+    }
+}
