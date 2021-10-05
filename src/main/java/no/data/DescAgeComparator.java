3b8bff995740027fb4266173dd370d3c93dad4fe
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-10-04 19:29:03 -0500

    :test: make test compile

---
+++ src/main/java/no/data/DescAgeComparator.java
@@ -0,0 +1,10 @@
+package no.data;
+
+import java.util.Comparator;
+
+public class DescAgeComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        return 0;
+    }
+}
